95ca9344d0953a7031e223102c0d28e59af3c7f3
Sascha Brawer <sascha@brawer.ch>
2016-09-01 15:50:37 +0200

    Emit prettier test reports

--- check.py
+++ check.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
 #
 # Copyright 2016 Unicode Inc. All rights reserved.
 #
@@ -13,39 +14,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import argparse, os, re, subprocess
+#
+from __future__ import unicode_literals
+import argparse, datetime, os, re, subprocess, time
 import xml.etree.ElementTree as etree
-
-
-def build():
-    subprocess.check_call(
-        './src/third_party/gyp/gyp -f make --depth . '
-        '--generator-output build  src/fonttest/fonttest.gyp'.split())
-    subprocess.check_call(['make', '-s', '--directory', 'build'])
-
-
-def check1(path, testcase, engine):
-    success = True
-    with open(path, 'r') as expected_file:
-        expected = expected_file.read()
-    expected_lines = [l + os.linesep for l in expected.splitlines()]
-    command = ['./build/out/Default/fonttest',
-               '--testcase=%s' % testcase,
-               '--fontdir=fonts',
-               '--engine=%s' % engine]
-    observed = subprocess.check_output(command)
-    observed_lines = [l + os.linesep for l in observed.splitlines()]
-    if expected_lines != observed_lines:
-        success = False
-        observed_path = path.replace('expected',
-                                     'observed-' + engine.replace('/', '-'))
-        print
-        for line in difflib.unified_diff(
-                expected_lines, observed_lines,
-                fromfile=path, tofile=observed_path):
-            print line,
-    return success
 
 
 FONTTEST_NAMESPACE = '{https://github.com/OpenType/fonttest}'
@@ -58,9 +30,18 @@ FONTTEST_VARIATION = FONTTEST_NAMESPACE + 'var'
 class ConformanceChecker:
     def __init__(self, engine):
         self.engine = engine
+        self.datestr = self.make_datestr()
+        self.reports = {}  # filename --> HTML ElementTree
+        self.conformance = {}  # testcase -> True|False
+        self.observed = {}  # testcase --> SVG ElementTree
+
+    def make_datestr(self):
+        now = datetime.datetime.now()
+        return '%s %d, %d' % (time.strftime("%B"), now.day, now.year)
 
     def check(self, testfile):
         doc = etree.parse(testfile).getroot()
+        self.reports[testfile] = doc
         for e in doc.findall(".//*[@class='expected']"):
             testcase = e.attrib[FONTTEST_ID]
             font = os.path.join('fonts', e.attrib[FONTTEST_FONT])
@@ -75,16 +56,75 @@ class ConformanceChecker:
             observed = subprocess.check_output(command)
             observed_svg = etree.fromstring(observed)
             self.normalize_svg(observed_svg)
+            self.observed[testcase] = observed_svg
             expected_str = \
                 etree.tostring(expected_svg, encoding='utf-8').strip()
             observed_str = \
                 etree.tostring(observed_svg, encoding='utf-8').strip()
-            print testcase, expected_str == observed_str
+            ok = (expected_str == observed_str)
+            self.conformance[testcase] = ok
+            # print testcase, 'PASS' if ok else 'FAIL'
+            groups = testcase.split('/')
+            for i in range(len(groups)):
+                group = '/'.join(groups[:i])
+                self.conformance[group] = (ok and
+                                           self.conformance.get(group, True))
 
     def normalize_svg(self, svg):
         strip_path = lambda p: re.sub(r'\s+', ' ', p).strip()
         for path in svg.findall(".//path[@d]"):
             path.attrib['d'] = strip_path(path.attrib['d'])
+
+    def write_report(self, path):
+        report = etree.parse('testcases/index.html').getroot()
+        report.find("./body/h2").text = self.datestr + ' · ' + self.engine
+        summary = report.find("./body//*[@id='SummaryText']")
+        fails = [k for k, v in self.conformance.items() if k and not v]
+        fails = sorted(set([t.split('/')[0] for t in fails]))
+        if len(fails) == 0:
+            summary.text = 'All tests have passed.'
+        else:
+            summary.text = 'Some tests have failed. See '
+            for f in fails:
+                if f is not fails[0]:
+                    if f is fails[-1]:
+                        etree.SubElement(summary, None).text = ', and '
+                    else:
+                        etree.SubElement(summary, None).text = ', '
+                link = etree.SubElement(summary, 'a')
+                link.text, link.attrib['href'] = f, '#' + f
+            etree.SubElement(summary, None).text = ' for details.'
+
+        head = report.find("./head")
+        for sheet in list(head.findall("./link[@rel='stylesheet']")):
+            href = sheet.attrib.get('href')
+            if href and '://' not in href:
+                internalStyle = etree.SubElement(head, 'style')
+                with open(os.path.join('testcases', href), 'r') as sheetfile:
+                    internalStyle.text = sheetfile.read().decode('utf-8')
+                head.remove(sheet)
+
+        for filename, doc in sorted(self.reports.items()):
+            for e in doc.findall(".//*[@class='observed']"):
+                e.append(self.observed.get(e.attrib[FONTTEST_ID]))
+            for e in doc.findall(".//*[@class='conformance']"):
+                if self.conformance.get(e.attrib[FONTTEST_ID]):
+                    e.text, e.attrib['class'] = '✓', 'conformance-pass'
+                else:
+                    e.text, e.attrib['class'] = '✖', 'conformance-fail'
+
+            for subElement in doc.find('body'):
+                report.find('body').append(subElement)
+
+        with open(path, 'w') as outfile:
+            outfile.write(etree.tostring(report, encoding='utf-8'))
+
+
+def build():
+    subprocess.check_call(
+        './src/third_party/gyp/gyp -f make --depth . '
+        '--generator-output build  src/fonttest/fonttest.gyp'.split())
+    subprocess.check_call(['make', '-s', '--directory', 'build'])
 
 
 def main():
@@ -92,12 +132,18 @@ def main():
     parser.add_argument('--engine',
                         choices=['FreeStack', 'CoreText', 'DirectWrite'],
                         default='FreeStack')
+    parser.add_argument('--output', help='path to report file being written')
     args = parser.parse_args()
     build()
     checker = ConformanceChecker(engine=args.engine)
     for filename in os.listdir('testcases'):
-        if not filename.endswith('.html'): continue
+        if (filename == 'index.html'
+                or not filename.endswith('.html')):
+            continue
         checker.check(os.path.join('testcases', filename))
+    print('PASS' if checker.conformance.get('') else 'FAIL')
+    if args.output:
+        checker.write_report(args.output)
 
 
 if __name__ == '__main__':
